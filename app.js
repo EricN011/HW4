54f4597e59c8095344da3f1606abaab9dbcabce7
Eric Noll <eanoll88@gmail.com>
2019-10-10 09:52:03 -0500

    quiz update

---
+++ app.js
@@ -0,0 +1,21 @@
+// variables
+// array of questions (question~string, choices~array, answer~string, key~number)
+// score
+// helper functions
+
+// helper functions
+
+// events
+// click highscore link
+// find object in local storage
+// parse object for screen
+// display parsed object in html
+
+// clear highscores
+// go back
+// start quiz
+// choose answer
+// enter initials and submit score, go to highscore event
+
+// init
+// none
