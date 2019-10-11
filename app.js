6165689f2a3a2f20505e1aff3e267f08d379b274
Eric Noll <eanoll88@gmail.com>
2019-10-11 12:22:45 -0500

    quiz update

--- app.js
+++ app.js
@@ -1,55 +1,44 @@
 // variables
-var header = document.getElementById("header");
-var scoreBtn = document.getElementById("scoreBtn");
-var timer = document.getElementById("timer");
-var container = document.getElementById("container");
 var start = document.getElementById("start");
-var quiz = document.getElementById("quiz");
-var question = document.getElementById("question");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var highScore = document.getElementById("highScores");
 
-start.addEventListener("click", function();
+start.addEventListener("click", startQuiz);
 function startQuiz() {
-  alert = "click!";
+  alert("click!");
 }
 // array of questions (question~string, choices~array, answer~string, key~number)
 var questions = [
   {
     question: "Question 1",
+    choices: ["False", "Correct", "False", "False"],
+    answer: "Correct"
+  },
+  {
+    question: "Question 2",
     answers: [
+      { Text: "False", correct: false },
       { Text: "False", correct: false },
       { Text: "Correct", correct: true },
       { Text: "False", correct: false }
     ]
   },
   {
-    question:
-      "Question 2",
+    question: "Question 3",
     answers: [
+      { Text: "False", correct: false },
       { Text: "False", correct: false },
       { Text: "False", correct: false },
       { Text: "Correct", correct: true }
     ]
   },
   {
-    question:
-    "Question 2",
-  answers: [
-    { Text: "False", correct: false },
-    { Text: "Correct", correct: true },
-    { Text: "False", correct: false }
-  },
-  {
-    question:
-    "Question 2",
-  answers: [
-    { Text: "Correct", correct: true },
-    { Text: "False", correct: false },
-    { Text: "False", correct: false }
-  },
+    question: "Question 4",
+    answers: [
+      { Text: "Correct", correct: true },
+      { Text: "False", correct: false },
+      { Text: "False", correct: false },
+      { Text: "False", correct: false }
+    ]
+  }
 ];
 
 // score
